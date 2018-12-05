16cb592162addff6dafd7c84209dde4f4f0c4359
sankar1061 <ecesankar93@gmail.com>
2018-12-05 15:29:21 +0530

    Update NovalnetRouteServiceProvider.php

--- src/Providers/NovalnetRouteServiceProvider.php
+++ src/Providers/NovalnetRouteServiceProvider.php
@@ -34,8 +34,8 @@ class NovalnetRouteServiceProvider extends RouteServiceProvider
         // Get the Novalnet success, cancellation and callback URLs
         $router->match(['post', 'get'],'payment/novalnet/redirectPayment' , 'Novalnet\Controllers\PaymentController@redirectPayment');
         $router->match(['post', 'get'],'payment/novalnet/callback', 'Novalnet\Controllers\CallbackController@processCallback');
-        $router->post('payment/novalnet/processPayment' , 'Novalnet\Controllers\PaymentController@processPayment' );
-        $router->post('payment/novalnet/paymentResponse' , 'Novalnet\Controllers\PaymentController@paymentResponse' );
+        $router->match(['post', 'get'],'payment/novalnet/processPayment' , 'Novalnet\Controllers\PaymentController@processPayment' );
+        $router->match(['post', 'get'],'payment/novalnet/paymentResponse' , 'Novalnet\Controllers\PaymentController@paymentResponse' );
         
     }
 }
